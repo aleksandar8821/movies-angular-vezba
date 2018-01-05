92114c484611bb6396e0f9b499c0d5cb48fb5b49
Aleksandar Foltin <88aleksandar88@gmail.com>
2018-01-05 21:21:04 +0100

    zadatak 3

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../shared/movie.service';
+import { Movie } from '../shared/models/movie';
+
 
 @Component({
   selector: 'app-movies',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesComponent implements OnInit {
 
-  constructor() { }
+	private movies: Array<Movie> //ovi iz vivify su ovde i inicijalizovali ovo na prazan niz, ne znam koliko to treba, meni radi i bez toga (https://gitlab.com/vivify-ideas/vivifyacademy-advanced-angular-movies/commit/bdb9ac5436c9f32728ef33e2549e350fa8e82002#b8d7997efd673eb17279a81145dc913f2fe4d39b)
+
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+  	this.movieService.getMovies().subscribe(data => {
+  		this.movies = data
+  		console.log(this.movies);
+  	})
+
+
   }
 
 }
--- src/app/shared/movie.service.ts
+++ src/app/shared/movie.service.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@angular/core';
+import { movieList } from './examples';
+import { Movie } from './models/movie';
+import { Observable, Observer } from 'rxjs/Rx';
+
+
 
 @Injectable()
 export class MovieService {
 
-  constructor() { }
+	private movieList: Array<Movie>;
+
+  constructor() {
+  	this.movieList = movieList.map((movie) => {
+  		return new Movie(
+  			movie.id,
+  			movie.name,
+  			movie.director,
+  			movie.imageUrl,
+  			movie.duration,
+  			new Date(movie.releseDate),
+  			movie.genres
+
+  		)
+  	})
+
+  }
+
+  public getMovies(){
+  	return new Observable((o: Observer<any>) => {
+  		o.next(this.movieList)
+  		return o.complete()
+  	})
+  }
+
+  // Ovako su uradili ovi iz vivify
+  // public getMovies():Observable<Movie[]>{
+  // 	return Observable.of(this.movieList)
+  // }
+
 
 }
