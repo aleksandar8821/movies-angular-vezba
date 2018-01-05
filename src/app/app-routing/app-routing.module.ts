0d77b405e82ef60713331d37c476754fed0f79e1
Aleksandar Foltin <88aleksandar88@gmail.com>
2018-01-05 02:07:16 +0100

    zadatak 1

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { MoviesComponent } from '../movies/movies.component';
+
+const appRoutes: Routes = [
+   {
+       path: '',
+       redirectTo: '/movies',
+       pathMatch: 'full'
+   },
+   {
+       path: 'movies',
+       component: MoviesComponent,
+   },
+];
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+	  RouterModule.forRoot(
+	    appRoutes
+	  )
+
+  ],
+  exports: [
+	  RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
